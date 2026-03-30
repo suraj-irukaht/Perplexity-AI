ddb9fe59f2e4aaf6da4540b32a7082e544b584a3
Suraj Shahi <surajshahi331@gmail.com>
2026-03-30 17:22:09 +0300

    update readme file

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,13 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.route.js";
-import { requestId } from "./middlewares/requestId.middleware.js";
-import { errorHandler } from "./middlewares/errorHandler.middleware.js";
+import { errorHandler } from "./middlewares/helper/errorHandler.middleware.js";
 const app = express();
 
 //Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(requestId);
+
 
 /**
  * - Routes used
--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -19,6 +19,10 @@ if (!process.env.CLIENT_SECRET) {
 if (!process.env.GMAIL_USER) {
   throw new Error("Gmail user is missing");
 }
+if (!process.env.APP_ORIGIN) {
+  throw new Error("App origin is missing");
+}
+
 
 const config = {
   MONGO_URI: process.env.MONGO_URI,
@@ -27,6 +31,10 @@ const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
   GMAIL_USER: process.env.GMAIL_USER,
+    APP_ORIGIN: process.env.APP_ORIGIN,
+    ACCESS_SECRET: process.env.ACCESS_SECRET,
+    REFRESH_SECRET: process.env.REFRESH_SECRET,
 };
+console.log(config.REFRESH_SECRET)
 
 export default config;
